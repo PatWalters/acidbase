2b55c8b0107680c998781eae66cd6b02c91cc695
Pat Walters <pat_walters@vrtx.com>
2014-07-08 09:15:57 -0400

    cleanup

--- drug_ionization/RUN.csh
+++
@@ -1,6 +0,0 @@
-#!/bin/csh
-
-../make_sdf.py chembl_drugs.smi chembl_drugs.sdf
-../charge_state.py chembl_drugs.sdf > chembl_drugs.charge_state
-./format_drug_pka.py 
-
--- drug_ionization/chembl_drugs_ionized_vs_ph.R
+++
@@ -1,5 +0,0 @@
-library(beeswarm)
-
-d = read.csv("chembl_drugs_ionized_vs_ph.csv",header=TRUE)
-d$COL = c("red","blue")[factor(d$STATE,levels=c("acidic","basic"))]
-beeswarm(IONIZED~pH,d,method="hex",pch=16,cex=0.4,pwc=d$COL,corral="gutter",ylab="% Ionized")
